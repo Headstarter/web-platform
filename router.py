ce31d106098ab518216aa35f867831faf993f8a3
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2018-12-23 00:34:21 +0200

    started with regionalization

--- router.py
+++ router.py
@@ -1,11 +1,27 @@
 from flask import Flask, render_template
 app = Flask (__name__)
 
-@app.route('/')
-def index():
+from flask_babel import Babel, gettext
+app.config.from_pyfile ('config.cfg')
+babel = Babel (app)
+
+from flask import g, request
+
+@babel.localeselector
+def get_locale():
+	translations = [str(translation) for translation in babel.list_translations()]
+	print (translations)
+	return request.accept_languages.best_match(translations)
+
+@app.route ('/')
+def index ():
    return render_template ('index.html')
 
-@app.route('/company/<companyId>', methods = ['GET', 'POST'])
+@app.route ('/company/<companyId>', methods = ['GET', 'POST'])
+def basic_company_profile_view (companyId):
+	return ''
+
+@app.route ('/companies', methods = ['GET', 'POST'])
 def basic_company_profile_view (companyId):
 	return ''
 
