cc42bb9886993d0d3d9479ba88e2e7da5594e2b2
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2019-02-23 20:18:39 +0200

    not finished yet

--- app/router.py
+++ app/router.py
@@ -1,12 +1,15 @@
 from app import app, babel, db, migrate, render_template
 from app.models import User, Sector, Company, Position
-from flask import g, request, session
+from flask import g, request, session, flash
 from flask_session import Session
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+app.config['SECRET_KEY'] = b'_5#y2L"F4Q8z\n\xec]/'
 
 # Check Configuration section for more details
-SESSION_TYPE = 'filesystem'
+SESSION_TYPE = 'raddit'
 app.config.from_object(__name__)
-Session(app)
+sess = Session(app)
 
 @babel.localeselector
 def get_locale():
@@ -22,13 +25,24 @@ def index ():
 def login ():
 	return render_template ('login.html')
 
-@app.route ('/login/student')
-def student_login ():
-	return render_template ('index.html')
-
-@app.route ('/login/company')
+@app.route ('/login/company', methods=['GET', 'POST'])
 def company_login ():
-	return render_template ('index.html')
+	if request.method == 'POST':
+		if len(User.query.filter (User.email==request.form ['email']).filter (User.type_registration==request.form ['type']).all ()) == 1:
+			session['email'] = request.form['email']
+			session['company_id'] = User.query.filter (User.email==request.form ['email']).filter (User.type_registration==request.form ['type']).all()[0].company_id
+			session['type'] = 'HR'
+
+			return redirect(url_for('index'))
+		else:
+			flash ('User is not registered or user is not login by the correct way.')
+			return redirect(url_for(company_login))
+	else:
+		return render_template ('company/login.html')
+
+@app.route ('/signup/company', methods=['GET', 'POST'])
+def company_signup ():
+	return render_template ('company/register.html')
 
 from app.v1pre.v1pre import visitors_routes, students_routes, companies_routes
 
