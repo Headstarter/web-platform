24b01ce0229895b7bad33f16b867c11698a2c7c8
Alex Tsvetanov <alex@alexts.tk>
2019-04-08 22:33:52 +0300

    first design changes based on AVI'S SALT

--- app/v1_1pre/visitors.py
+++ app/v1_1pre/visitors.py
@@ -1,24 +1,23 @@
 from app import render_template, flash
 from app.router import session
-from flask import request, redirect
+from flask import request, redirect, url_for
 from app.models import User, Company, Position, Tag, Application, insert_application
-
+import sys
 
 class Visitors:
 
     @staticmethod
     def homepage():
         return render_template('visitor/homepage.html',
-                               positions=Position.query.filter(Position.available==True).order_by(
+                               positions=Position.query.filter(Position.available == True).order_by(
                                          Position.id.desc()).limit(5),
                                companies=Company.query.all())
-
 
     @staticmethod
     def apply_student(position_id):
         if session['type'] != 'Student':
             session['redirect'] = request.full_path
-            return redirect('/login/student')
+            return redirect(url_for('student_signup'))
         else:
             #flash(position_id, 'info')
             #flash(len(Position.query.filter(id==position_id).all()), 'info')
@@ -34,73 +33,56 @@ class Visitors:
         offers_per_page = 10
         position = -2
         if request.form.get('position'):
-            position = int(request.form.get('position')) - 1
+            position = int(request.form.get('position'))
+
+        company_id = -1
+        if request.form.get('company'):
+            company_id = int(request.form.get('company'))
 
         if position == -2:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .limit(offers_per_page)
-                                   .offset(page * offers_per_page),
-                                   tags=Tag.query.filter(True).all())
+            positions = Position.query.filter(Position.available == True)
 
-        elif position == 0:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .filter(Position.tag_id < 9)
-                                   .limit(offers_per_page)
-                                   .offset(page * offers_per_page),
-                                   tags=Tag.query.filter(True).all())
+        elif position == 1:
+            positions = Position.query.filter(Position.available == True)\
+                                   .filter(Position.tag_id <= 9)
 
         elif position == 10:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .filter(Position.tag_id > 8)
-                                   .filter(Position.tag_id < 14)
-                                   .limit(offers_per_page)
-                                   .offset(page * offers_per_page),
-                                   tags=Tag.query.filter(True).all())
+            positions = Position.query.filter(Position.available == True)\
+                                   .filter(Position.tag_id > 9)\
+                                   .filter(Position.tag_id <= 14)
 
-        elif position == 14:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .filter(Position.tag_id > 13)
-                                   .filter(Position.tag_id < 21)
-                                   .limit(offers_per_page)
-                                   .offset(page * offers_per_page),
-                                   tags=Tag.query.filter(True).all())
+        elif position == 15:
+            positions = Position.query.filter(Position.available == True)\
+                                   .filter(Position.tag_id > 14)\
+                                   .filter(Position.tag_id <= 21)
 
-        elif position == 21:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .filter(Position.tag_id > 21)
-                                   .filter(Position.tag_id < 28)
-                                   .limit(offers_per_page)
-                                   .offset(page * offers_per_page),
-                                   tags=Tag.query.filter(True).all())
+        elif position == 22:
+            positions = Position.query.filter(Position.available == True)\
+                                   .filter(Position.tag_id > 21)\
+                                   .filter(Position.tag_id <= 28)
 
-        elif position == 28:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .filter(Position.tag_id > 28)
-                                   .filter(Position.tag_id < 32)
-                                   .limit(offers_per_page)
-                                   .offset(page * offers_per_page),
-                                   tags=Tag.query.filter(True).all())
-
-        elif position == 39:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .filter(Position.tag_id > 39)
-                                   .limit(offers_per_page)
-                                   .offset(page * offers_per_page),
-                                   tags=Tag.query.filter(True).all())
+        elif position == 29:
+            positions = Position.query.filter(Position.available == True)\
+                                   .filter(Position.tag_id > 28)\
+                                   .filter(Position.tag_id <= 32)
         else:
-            return render_template('visitor/browse.html',
-                                   positions=Position.query.filter(Position.available == True)
-                                   .filter(Position.tag_id == position + 1),
-                                   tags=Tag.query.filter(True).all())
+            positions = Position.query.filter(Position.available == True)
 
-    @staticmethod
-    def all_positions():
-        return render_template('visitor/positions.html',
-                               positions=Position.query.filter(Position.available==True))
+        print(positions.all(), file=sys.stderr)
+        if company_id != -1:
+            print(company_id, file=sys.stderr)
+            positions = positions.filter(Position.company_id == company_id)
+
+        print(positions.all(), file=sys.stderr)
+        positions = positions.order_by(Position.id.desc()).all()
+
+        print(positions, file=sys.stderr)
+
+        if len(positions) == 0:
+            flash('За момента няма стажове за Вас.', 'warning')
+            flash('Потърсете пак по-късно.', 'info')
+
+        return render_template('visitor/browse.html',
+                               positions=positions,
+                               tags=Tag.query.all(),
+                               companies=Company.query.all())
