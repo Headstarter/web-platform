c4147db2c2b285bf5f34592396f00a373cf9049f
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2019-06-10 00:00:17 +0300

    pre-merge changes

--- app/static/js/cv.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-    $("ul.list.form-control li:not(.add)").append(`
-        <a onclick="$(this).parent('li').remove();" class="btn btn-danger btn-raised">Изтрии</a><br>
-    `);
-});
