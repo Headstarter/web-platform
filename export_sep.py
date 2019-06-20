a6b3419443241c801d130d2fa5afbd1dc9c00f92
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2019-06-20 19:35:06 +0300

    db recovery

--- export_sep.py
+++ export_sep.py
@@ -1,17 +1,103 @@
 import sqlite3
 import csv
 
-con = sqlite3.connect('app/app_old.db')
+from numpy import genfromtxt
+from time import time
+from datetime import datetime
+from sqlalchemy import Column, Integer, Float, Date, String, VARCHAR
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import csv
+import pandas as pd
+from sqlalchemy import Column, Integer, String, Boolean
 
-for table in ['User', 'Mapper', 'Tag', 'Company', 'Verify','CV','Position']:
-	outfile = open('backup/production_data_' + table + '.csv', 'w')
-	outcsv = csv.writer(outfile)
-	cursor = con.execute('select * from ' + table)
+tables = ['User', 'Mapper', 'Tag', 'Company',
+          'Verify', 'CV', 'Position']#, 'Application']
 
-# dump column titles (optional)
-	print(len(cursor.description))
-	outcsv.writerows([cursor.description[x] for x in range(len(cursor.description))])
-# dump rows
-	outcsv.writerows(cursor.fetchall())
 
-outfile.close()
+def export():
+    con = sqlite3.connect('app/app_old.db')
+
+    for table in tables:
+        outfile = open('backup/production_data_' + table + '.csv', 'w')
+        outcsv = csv.writer(outfile)
+        cursor = con.execute('select * from ' + table)
+
+        # dump column titles (optional)
+        # print(str(len(cursor.description)))
+        # outcsv.writerows([cursor.description[x] for x in range(len(cursor.description))].join(', '))
+        # dump rows
+        outcsv.writerows(cursor.fetchall())
+
+    outfile.close()
+
+
+def recover():
+	engine = create_engine('sqlite:///app/app_new.db')
+	con = engine.connect()
+ 
+	import app.models as db_tables
+	db_tables.Base.metadata.create_all(engine)
+
+	for table in tables:
+		filename = 'backup/production_data_' + table + '.csv'
+		print(table, ':')
+		print(filename, ':')
+		print([column.key for column in db_tables.factory(table).__table__.columns])
+		print([column.type for column in db_tables.factory(table).__table__.columns])
+		
+		types = [column.type for column in db_tables.factory(table).__table__.columns]
+
+		with open(filename, 'r', encoding="utf8") as f:
+			r = csv.reader(f, delimiter=',', quotechar='"')
+			data = []
+			counter = 0
+			for row in r:
+				counter = counter + 1
+				if counter <= len(types):
+					continue
+				t = tuple(row)
+				d = []
+				for x in range(len(types)):
+					print(t[x])
+					d.append('')
+					if t[x] == '':
+						if isinstance(types[x], Integer):
+							d[x] = int(0)
+						elif isinstance(types[x], String):
+							d[x] = str('')
+						elif isinstance(types[x], Boolean):
+							d[x] = bool(True)
+					else:
+						if isinstance(types[x], Integer):
+							d[x] = int(t[x])
+						elif isinstance(types[x], String):
+							d[x] = str(t[x])
+						elif isinstance(types[x], Boolean):
+							d[x] = bool(t[x])
+				data.append(tuple(d))
+				
+			print(data)
+			import sqlalchemy
+			ins = sqlalchemy.sql.expression.insert(db_tables.factory(table), data, bind=engine)
+			#print('------------------------\n', str(ins), '\n', ins.compile().params)
+			con.execute(ins)
+
+def exportOut(name):
+    con = sqlite3.connect('app/' + name)
+
+    for table in tables:
+        cursor = con.execute('select * from ' + table)
+
+        # dump column titles (optional)
+        # print(str(len(cursor.description)))
+        # outcsv.writerows([cursor.description[x] for x in range(len(cursor.description))].join(', '))
+        # dump rows
+        print(cursor.fetchall())
+
+    #outfile.close()
+
+
+recover()
+exportOut('app_new.db')
