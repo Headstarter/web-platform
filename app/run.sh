9c0139d4cd35c95d1004ff27912eaf09f4b62769
Alex Tsvetanov <alex@alexts.tk>
2019-08-19 10:35:52 +0300

    views and school registration

--- app/run.sh
+++ app/run.sh
@@ -4,3 +4,5 @@ flask db upgrade
 flask db migrate
 echo -ne "from app import clear, init\nclear()\ninit()\n" | python
 flask run
+
+https://www.linkedin.com/uas/oauth2/authorization?response_type=code&client_id=8672qjr3q74rgx&client_secret=AAEZwwiRpRNILccH&redirect_uri=https://headstarter.eu&scope=r_fullprofile%20r_emailaddress%20w_share&state=DCEeFWf45A53sdfKef424state=DCEeFWf45A53sdfKef424
