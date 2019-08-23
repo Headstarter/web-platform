b20dd4c35e24989510da6e9de3128bd1e5121cde
Alex Tsvetanov <atsvetanov@codixfr.private>
2019-08-23 11:45:46 +0300

    fixes

--- app/v1/helpers/mailer.py
+++ app/v1/helpers/mailer.py
@@ -4,12 +4,13 @@ from app.router import app
 from app.models import Verify, User, db
 import os
 
-app.config['MAIL_SERVER'] = 'appssmtp.abv.bg'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = os.environ['EMAILUSER']
-app.config['MAIL_PASSWORD'] = os.environ['EMAILPASS']
-app.config['MAIL_USE_TLS'] = False
-app.config['MAIL_USE_SSL'] = True
+if os.environ['DEBUG'] == 'off':
+    app.config['MAIL_SERVER'] = 'appssmtp.abv.bg'
+    app.config['MAIL_PORT'] = 465
+    app.config['MAIL_USERNAME'] = os.environ['EMAILUSER']
+    app.config['MAIL_PASSWORD'] = os.environ['EMAILPASS']
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
 
 
@@ -27,18 +28,20 @@ class Mailer:
         
     @staticmethod
     def sendConfirmation(new_user):
-        verify = Mailer.get_verification(new_user)
-        msg = Message('Confirm your registration in headstarter.eu', sender='Headstarter Corporation <' + os.environ['EMAILUSER'] + '>', recipients=[new_user.email, 'headstarter@headstarter.eu'])
-        msg.html = render_template('reg_confirm.html', link='https://headstarter.eu/verify/' + verify.code)
-        mail.send(msg)
-        return msg.html
+        if os.environ['DEBUG'] == 'off':
+            verify = Mailer.get_verification(new_user)
+            msg = Message('Confirm your registration in headstarter.eu', sender='Headstarter Corporation <' + os.environ['EMAILUSER'] + '>', recipients=[new_user.email, 'headstarter@headstarter.eu'])
+            msg.html = render_template('reg_confirm.html', link='https://headstarter.eu/verify/' + verify.code)
+            mail.send(msg)
+            return msg.html
     
     @staticmethod
     def sendResetPassword(user):
-        verify = Mailer.get_verification(user)
-        msg = Message('Reset your password in headstarter.eu', sender='Headstarter Corporation <' + os.environ['EMAILUSER'] + '>', recipients=[user.email, 'headstarter@headstarter.eu'])
-        msg.html = render_template('reset_password.html', link='https://headstarter.eu/reset/' + verify.code)
-        mail.send(msg)
-        return msg.html
+        if os.environ['DEBUG'] == 'off':
+            verify = Mailer.get_verification(user)
+            msg = Message('Reset your password in headstarter.eu', sender='Headstarter Corporation <' + os.environ['EMAILUSER'] + '>', recipients=[user.email, 'headstarter@headstarter.eu'])
+            msg.html = render_template('reset_password.html', link='https://headstarter.eu/reset/' + verify.code)
+            mail.send(msg)
+            return msg.html
         
 
