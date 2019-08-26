6c3943c3da8ed75f76b35c0729f1f3bc2c37538b
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2019-08-26 13:47:43 +0300

    data for migration and new migration script that generates python file for the migration

--- export_sep.py
+++ export_sep.py
@@ -12,92 +12,30 @@ import csv
 import pandas as pd
 from sqlalchemy import Column, Integer, String, Boolean
 
-tables = ['User', 'Mapper', 'Tag', 'Company',
-          'Verify', 'CV', 'Position']#, 'Application']
+tables = ['User', 'Mapper', 'Tag', 'Company', 'Approval',
+          'Verify', 'CV', 'Position', 'Application', 'School']
+variables = ['_users', '_mappers', '_tags', '_companies', '_approvals', 
+             '_verifies', '_cvs', '_offers', '_applications', 'schools']
 
-
-def export():
-    con = sqlite3.connect('app/app_old.db')
-
-    for table in tables:
-        outfile = open('backup/production_data_' + table + '.csv', 'w')
-        outcsv = csv.writer(outfile)
-        cursor = con.execute('select * from ' + table)
-
-        # dump column titles (optional)
-        # print(str(len(cursor.description)))
-        # outcsv.writerows([cursor.description[x] for x in range(len(cursor.description))].join(', '))
-        # dump rows
-        outcsv.writerows(cursor.fetchall())
-
-    outfile.close()
-
-
-def recover():
-	engine = create_engine('sqlite:///app/app_new.db')
-	con = engine.connect()
- 
-	import app.models as db_tables
-	db_tables.Base.metadata.create_all(engine)
-
-	for table in tables:
-		filename = 'backup/production_data_' + table + '.csv'
-		print(table, ':')
-		print(filename, ':')
-		print([column.key for column in db_tables.factory(table).__table__.columns])
-		print([column.type for column in db_tables.factory(table).__table__.columns])
-		
-		types = [column.type for column in db_tables.factory(table).__table__.columns]
-
-		with open(filename, 'r', encoding="utf8") as f:
-			r = csv.reader(f, delimiter=',', quotechar='"')
-			data = []
-			counter = 0
-			for row in r:
-				counter = counter + 1
-				if counter <= len(types):
-					continue
-				t = tuple(row)
-				d = []
-				for x in range(len(types)):
-					print(t[x])
-					d.append('')
-					if t[x] == '':
-						if isinstance(types[x], Integer):
-							d[x] = int(0)
-						elif isinstance(types[x], String):
-							d[x] = str('')
-						elif isinstance(types[x], Boolean):
-							d[x] = bool(True)
-					else:
-						if isinstance(types[x], Integer):
-							d[x] = int(t[x])
-						elif isinstance(types[x], String):
-							d[x] = str(t[x])
-						elif isinstance(types[x], Boolean):
-							d[x] = bool(t[x])
-				data.append(tuple(d))
-				
-			print(data)
-			import sqlalchemy
-			ins = sqlalchemy.sql.expression.insert(db_tables.factory(table), data, bind=engine)
-			#print('------------------------\n', str(ins), '\n', ins.compile().params)
-			con.execute(ins)
 
 def exportOut(name):
-    con = sqlite3.connect('app/' + name)
+	con = sqlite3.connect('app/' + name)
 
-    for table in tables:
-        cursor = con.execute('select * from ' + table)
+	print('from app.models import db, User, Mapper, Tag, Company, Verify, CV, Position, School, Approval')
 
-        # dump column titles (optional)
-        # print(str(len(cursor.description)))
-        # outcsv.writerows([cursor.description[x] for x in range(len(cursor.description))].join(', '))
-        # dump rows
-        print(cursor.fetchall())
+	for x in range(len(tables)):
+		table = tables[x]
+		var = variables[x]
+		cursor = con.execute('select * from ' + table)
+		print(variables, '=', cursor.fetchall())
+        
+		print('for x in', var + ':')
+		print('	try:')
+		print('		db.session.add(' + table + '(*x))')
+		print('		db.session.commit()')
+		print('	except Exception as e:')
+		print('		db.session.rollback()')
+		print('		print(str(e), \'\n\', x)')
 
-    #outfile.close()
 
-
-# recover()
-# exportOut('app_new.db')
+exportOut('app.db')
