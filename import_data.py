4ed72a93b021f6c25660570f4355e9428e4bf988
Alex Tsvetanov <14363901+Alex-Tsvetanov@users.noreply.github.com>
2019-09-02 15:16:13 +0300

    import files - description not found

--- import_data.py
+++ import_data.py
@@ -1,21 +1,22 @@
 import json
 from app.models import *
+import os
 
 lineList = list()
-with open('toimport.txt', 'r', encoding='utf-8') as f:
+with open('toimport_edited.txt', 'r', encoding='utf-8') as f:
     for line in f:
         if line.find('Job') != -1: # init job line
             pass
-        elif line.startswith('job'): # finialize job
+        elif line.find('Generated') != -1: # finialize job
             obj = {}
             try:
                 import re
                 obj_str = ''.join(lineList)
+                obj_str = re.sub(r'description:    ', r'description: ', obj_str)
                 obj_str = re.sub(r'"javascript:', r'Undefined', obj_str)
-                obj_str = re.sub(r' [\']?([a-z-_]*)[\']?:', r' "\1":', obj_str)
+                obj_str = re.sub(r' [\']?([A-Za-z-_]*)[\']?:', r' "\1":', obj_str)
                 obj_str = re.sub(r': \'(.*?)\',', r': "\1",', obj_str)
                 obj_str = re.sub(r'\'58\'', r'"58"', obj_str)
-                obj_str = re.sub(r'"description":  ', r'"description":  "",', obj_str)
                 obj_str = re.sub(r'job-title', r'name', obj_str)
                 obj_str = re.sub(r'job-age', r'age_required', obj_str)
                 obj_str = re.sub(r'job-type', r'hours_per_day', obj_str)
@@ -31,10 +32,13 @@ with open('toimport.txt', 'r', encoding='utf-8') as f:
             obj['views'] = '0'
             import datetime as DT
             obj['date'] = '{}'.format(DT.datetime.now())
+            print(obj_str)
             print(obj)
             offer = Position(**obj)
             db.session.add(offer)
             db.session.commit()
             lineList.clear()
         else:
+            if line.find('description') != -1:
+                print('Adding', line)
             lineList.append(line.rstrip())
