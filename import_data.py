8fa062b0d96a37ddccc3d42bf7a4a12c4decff24
Alex Tsvetanov <alex@alexts.tk>
2019-09-03 13:20:32 +0300

    random

--- import_data.py
+++ import_data.py
@@ -1,44 +1,79 @@
+# -*- coding: utf-8 -*-
+
 import json
 from app.models import *
 import os
+import re
 
 lineList = list()
 with open('toimport_edited.txt', 'r', encoding='utf-8') as f:
-    for line in f:
-        if line.find('Job') != -1: # init job line
-            pass
-        elif line.find('Generated') != -1: # finialize job
-            obj = {}
-            try:
-                import re
-                obj_str = ''.join(lineList)
-                obj_str = re.sub(r'description:    ', r'description: ', obj_str)
-                obj_str = re.sub(r'"javascript:', r'Undefined', obj_str)
-                obj_str = re.sub(r' [\']?([A-Za-z-_]*)[\']?:', r' "\1":', obj_str)
-                obj_str = re.sub(r': \'(.*?)\',', r': "\1",', obj_str)
-                obj_str = re.sub(r'\'58\'', r'"58"', obj_str)
-                obj_str = re.sub(r'job-title', r'name', obj_str)
-                obj_str = re.sub(r'job-age', r'age_required', obj_str)
-                obj_str = re.sub(r'job-type', r'hours_per_day', obj_str)
-                obj_str = re.sub(r'job-category', r'tag_id', obj_str)
-                obj_str = re.sub(r'job-available', r'available', obj_str)
-                obj_str = re.sub(r'false', r'true', obj_str)
-                           
-                obj = json.loads(obj_str)
-            except Exception as e:
-                print(e, obj_str)
-                break
-            obj['id'] = line.split(' ')[1].split(':')[0]
-            obj['views'] = '0'
-            import datetime as DT
-            obj['date'] = '{}'.format(DT.datetime.now())
-            print(obj_str)
-            print(obj)
-            offer = Position(**obj)
-            db.session.add(offer)
-            db.session.commit()
-            lineList.clear()
+    #while True: 
+    #    c = f.read(1)
+    #    if not c:
+    #        break
+    #    print(c, end='')
+    line = ''
+    while True: 
+        c = f.read(1)
+        if not c:
+            break
+        if c == '\n':
+            line = line.rstrip()
+            if re.match(r"^  description:    '(.*)',", line): # init job line
+                group, correct_group = re.match(r"^  description:    '(.*)',", line).group(1), re.match(r"^  description:    '(.*)',", line).group(1)
+                correct_group = correct_group.replace('\\"', '\\\\"')
+                correct_group = correct_group.replace('"', '\\"')
+                correct_group = correct_group.replace("\\'", '\\"')
+                correct_group = correct_group.replace('\\n', '')
+                correct_group = correct_group.replace('\\t', '')
+                correct_group = correct_group.replace('\\r', '')
+                correct_group = correct_group.replace('\\u001f', '')
+                correct_group = correct_group.replace('\\u0003', '')
+                correct_group = correct_group.replace('javascript', 'Undefined')
+                line = re.sub(r'%s' % re.escape(group), r'%s' % correct_group, line)
+                line = re.sub(r'^  description:    \'(.*)\',', r' "description":    "\1",', line)
+                lineList.append(line.rstrip())
+            elif re.match(r"^.*Job [0-9]+:$", line): # init job line
+                print('Starting gathering')
+                line = ''
+            elif re.match(r"^job [0-9]+: Generated$", line): # finialize job
+                obj = {}
+                flag = True
+                try:
+                    obj_str = ''.join(lineList)
+                    obj = json.loads(obj_str)
+                except Exception as e:
+                    print(e, obj_str)
+                    flag = False
+                if flag:
+                    obj['id'] = line.split(' ')[1].split(':')[0]
+                    obj['views'] = '0'
+                    import datetime as DT
+                    obj['date'] = '{}'.format(DT.datetime.now())
+                    print(obj_str)
+                    print(obj)
+                    offer = Position(**obj)
+                    db.session.add(offer)
+                    db.session.commit()
+                lineList.clear()
+            else:  
+                line = line.rstrip()
+                line = re.sub(r'^\s[\']?([A-Za-z-_]*)[\']?:', r' "\1":', line)
+                line = re.sub(r': \'(.*?)\',', r': "\1",', line)
+                line = re.sub(r'\'58\'', r'"58"', line)
+                line = re.sub(r'\'job-title\':', r'"name":', line)
+                line = re.sub(r'\'job-age\':', r'"age_required":', line)
+                line = re.sub(r'\'job-type\':', r'"hours_per_day":', line)
+                line = re.sub(r'\'job-category\':', r'"tag_id":', line)
+                line = re.sub(r'\'job-available\':', r'"available":', line)
+                line = re.sub(r'duration:', r'"duration":', line)
+                line = re.sub(r'email:', r'"email":', line)
+                line = re.sub(r'company_id:', r'"company_id":', line)
+                line = re.sub(r'id:', r'"id":', line)
+                line = re.sub(r'location:', r'"location":', line)
+                line = re.sub(r'false', r'true', line)
+                line = re.sub(r'""javascript:"', r'"Undefined"', line)
+                lineList.append(line.rstrip())
+            line = ''
         else:
-            if line.find('description') != -1:
-                print('Adding', line)
-            lineList.append(line.rstrip())
+            line = line + c
