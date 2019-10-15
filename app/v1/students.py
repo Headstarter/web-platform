ef6bb7a263130d9d9b93322c6aba60e5dabd31cc
Aleks Tsvetanov <smg73EE@smg.local>
2019-10-15 11:05:56 +0300

    hotfix

--- app/v1/students.py
+++ app/v1/students.py
@@ -20,16 +20,16 @@ def allowed_image(filename):
 
 class Students:
 	@staticmethod
-    def homepage():
-        return render_template('core/' + str(session['language'] or get_locale()) + '/' + Visitors.folder() + '/index.html',
-                               tags=Tag.query.all(),
-                               number_offers=Position.query.filter(
-                                   Position.available == True).count(),
-                               open=Target_Group.groupTags(),
-                               positions=Position.query.filter(
-                                   Position.available == True)
-                               .order_by(Position.id.desc())
-                               .limit(5))
+	def homepage():
+		return render_template('core/' + str(session['language'] or get_locale()) + '/' + Visitors.folder() + '/index.html',
+								tags=Tag.query.all(),
+								number_offers=Position.query.filter(
+									Position.available == True).count(),
+								open=Target_Group.groupTags(),
+								positions=Position.query.filter(
+									Position.available == True)
+								.order_by(Position.id.desc())
+								.limit(5))
 
 
 	@staticmethod
